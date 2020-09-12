d5bf6d3674aaa7d1b61d5f0f7484e28c1487ba09
ceaumo02 <ionlinesolutions.com@gmail.com>
2020-09-11 20:00:41 -0500

    Easier code

--- aproximacion.py
+++ aproximacion.py
@@ -4,19 +4,19 @@ objetivo = int(input('Escoge un número: '))
 epsilon = 0.0001
 paso = epsilon**2
 respuesta = 0.0
-num = 0
+# num = 0
 
 start = time.time()
 
 while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
     # print(abs(respuesta**2 - objetivo), respuesta)
     respuesta += paso
-    num += 1
+    # num += 1
 
-end = time.time()
-tiempo = round((end - start), 2)
+# end = time.time()
+# tiempo = round((end - start), 2)
 
-print(f'Para encontrár la respuesta con margen de error de {epsilon*100}%, hizo {num} iteraciones y duró {tiempo} segundos')
+#print(f'Para encontrár la respuesta con margen de error de {epsilon*100}%, hizo {num} iteraciones y duró {tiempo} segundos')
 
 if abs(respuesta**2 - objetivo) >= epsilon:
     print(f'No se encontró la raíz cuadrada de {objetivo}')
